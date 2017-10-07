cec28a4f82a744347d03a7d68c526d681ef5d636
Graham Lawing <graham.lawing@gmail.com>
2017-10-06 21:43:11 -0400

    created navbar

--- spir/src/components/Navbar.js
+++ spir/src/components/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 
+
 const Navbar = () => 
-<div>Navbar</div>;
-  
+
+<nav className="navbar navbar-dark bg-dark">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="#">SpIr</a>
+    </div>
+    <ul className="nav navbar-nav">
+      <li className="active"><a href="#">Home</a></li>
+    </ul>
+  </div>
+
+
+</nav>
+
 
 
   export default Navbar;
